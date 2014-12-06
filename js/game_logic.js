90c63d0048af92e8c714b80a504476790c0b9938
Mohammed Header <mohammed.header@espace.com.eg>
2014-12-06 16:02:08 +0200

    Move Characters By Block

--- js/game_logic.js
+++ js/game_logic.js
@@ -1,23 +1,50 @@
+function Character (name,column) {
+    this.name = name;
+    this.column = column;
+    this.row = 0;
+    this.targetBlock = this.row;
+}
+Character.prototype.setImage = function(image) {
+    this.image = image;
+};
+Character.prototype.getImage = function(image) {
+    return this.image;
+};
+Character.prototype.getColumn = function(image) {
+    return this.column;
+};
+Character.prototype.getName = function() {
+    return this.name;
+};
+Character.prototype.getRow = function() {
+    return this.row;
+};
+Character.prototype.addOneRow = function() {
+    this.row += 1;
+};
+Character.prototype.getTargetBlock = function() {
+    return this.targetBlock;
+};
+
+Character.prototype.setTargetBlock = function(_targetBlock) {
+    this.targetBlock = _targetBlock;
+};
+
 function getImageFactory(image_type){
 	switch(image_type) {
-    case "g":
-        return new createjs.Bitmap("sprites/GrassBlock.png");
-        break;
-    case "s":
-        return new createjs.Bitmap("sprites/DirtBlock.png");
-        break;
-    case "b":
-    		return new createjs.Bitmap("sprites/BrownBlock.png");
-    		break;
-    default:
-        return new createjs.Bitmap("sprites/BrownBlock.png");
+	    case "g":
+	        return new createjs.Bitmap("sprites/GrassBlock.png");
+	    case "s":
+	        return new createjs.Bitmap("sprites/DirtBlock.png");
+	    case "b":
+	    	return new createjs.Bitmap("sprites/BrownBlock.png");
+	    default:
+	        return new createjs.Bitmap("sprites/BrownBlock.png");
 	}
 }
-
-
-// var canvas = document.getElementById('game');
- var stage = new createjs.Stage("game");
-// var context = canvas.getContext('2d')//.fillRect(4,4,5,60);
+var blockSize = getImageFactory("1");
+var players = [new Character("Ahmed",0),new Character("Mohamed",1)];
+var stage = new createjs.Stage("game");
 var tileSize = 30;
 var map = [
 					["g","s","b","b","b","b","b","b"],
@@ -40,7 +67,6 @@ var map = [
 
 var drawTileBlock = function(image_type, x, y){
 	var block = getImageFactory(image_type);
-	console.log(block)
 	block.x = x * 100;
 	block.y = y * 85;
 	stage.addChild(block)
@@ -55,12 +81,16 @@ var drawMap = function(){
   });
 };
 
-var girl;
 var drawCharacters = function(){
-	girl = new createjs.Bitmap("sprites/CharacterPinkGirl.png");
-	girl.x = 0;
-	girl.y = 50;
-	stage.addChild(girl)
+	for (var i = players.length - 1; i >= 0; i--) {
+		var player = players[i]
+		console.log(player)
+		var image = new createjs.Bitmap("sprites/CharacterPinkGirl.png");
+		image.x = player.getColumn() * 100;
+		image.y = -50;
+		player.setImage(image)
+		stage.addChild(image)
+	};
 }
 
 var init = function(){
@@ -68,14 +98,23 @@ var init = function(){
 	drawCharacters();
 };
 
-
-
-
 createjs.Ticker.addEventListener("tick", handleTick);
  function handleTick(event) {
-     girl.y += 5;
-     if(girl.y > 150) girl.y = 150
+ 	for (var i = players.length - 1; i >= 0; i--) {
+ 		var player = players[i]
+ 		var image =  player.getImage()
+ 		var target_y = player.getTargetBlock() * blockSize.getBounds().height / 2 - 50;
+
+ 		var image = player.getImage();
+ 		image.y += 10;
+ 		if(image.y >=target_y ) image.y = target_y
+ 	};
      stage.update();
  }
 
 init();
+
+var advanceOneStep = function(player){
+	var character = players[player]
+	character.setTargetBlock(character.getTargetBlock() + 1)
+}
