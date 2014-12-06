d94b73b4056fc86ece051e4d6af9ba760a08cd4c
ahmed-shehata <ahmed.shehata@espace.com.eg>
2014-12-06 16:11:01 +0200

    Added Character class

---
+++ js/Character.js
@@ -0,0 +1,31 @@
+function Character (name,column) {
+    this.name = name;
+    this.column = column;
+    this.row = 0;
+    this.targetBlock = this.row;
+}
+Character.prototype.setImage = function(image) {
+    this.image = image;
+};
+Character.prototype.getImage = function(image) {
+    return this.image;
+};
+Character.prototype.getColumn = function(image) {
+    return this.column;
+};
+Character.prototype.getName = function() {
+    return this.name;
+};
+Character.prototype.getRow = function() {
+    return this.row;
+};
+Character.prototype.addOneRow = function() {
+    this.row += 1;
+};
+Character.prototype.getTargetBlock = function() {
+    return this.targetBlock;
+};
+
+Character.prototype.setTargetBlock = function(_targetBlock) {
+    this.targetBlock = _targetBlock;
+};
--- js/game_logic.js
+++ js/game_logic.js
@@ -1,35 +1,3 @@
-function Character (name,column) {
-    this.name = name;
-    this.column = column;
-    this.row = 0;
-    this.targetBlock = this.row;
-}
-Character.prototype.setImage = function(image) {
-    this.image = image;
-};
-Character.prototype.getImage = function(image) {
-    return this.image;
-};
-Character.prototype.getColumn = function(image) {
-    return this.column;
-};
-Character.prototype.getName = function() {
-    return this.name;
-};
-Character.prototype.getRow = function() {
-    return this.row;
-};
-Character.prototype.addOneRow = function() {
-    this.row += 1;
-};
-Character.prototype.getTargetBlock = function() {
-    return this.targetBlock;
-};
-
-Character.prototype.setTargetBlock = function(_targetBlock) {
-    this.targetBlock = _targetBlock;
-};
-
 var blockSize = getImageFactory("1");
 var players = [new Character("Ahmed",0),new Character("Mohamed",1)];
 
@@ -75,10 +43,11 @@ var drawTileBlock = function(image_type, x, y){
 	var block = getImageFactory(image_type);
 	block.x = x * 100;
 	block.y = y * 85 + mapMargin;
-	stage.addChild(block)
-}
+	stage.addChild(block);
+};
 
 var drawMap = function() {
+	 stage.canvas.width = window.innerWidth;
     _(map).each(function(row, i) {
         _(row).each(function(tile, j) {
             self.drawTileBlock(tile, i, j); //draw a rectangle at j,i
@@ -88,20 +57,20 @@ var drawMap = function() {
 
 var drawCharacters = function(){
 	for (var i = players.length - 1; i >= 0; i--) {
-		var player = players[i]
-		console.log(player)
+		var player = players[i];
+		console.log(player);
 		var image = new createjs.Bitmap("sprites/CharacterPinkGirl.png");
 		image.x = player.getColumn() * 100;
 		image.y = -50 + mapMargin;
-		player.setImage(image)
-		stage.addChild(image)
-	};
-}
+		player.setImage(image);
+		stage.addChild(image);
+	}
+};
 
 var drawGradient = function() {
     var canvas = document.getElementById('game');
     var context = new createjs.Rectangle(0, 0, canvas.width, canvas.height);
-    var graphicsShape = new createjs.Graphics().beginLinearGradientFill(["rgba(25,225,255,1)", "rgba(255,255,255,1)"], [0, 1], 0, 0, 0, 130).drawRoundRect(0, 0, canvas.width, canvas.height, 5);
+    var graphicsShape = new createjs.Graphics().beginLinearGradientFill(["rgba(25,225,255,1)", "rgba(255,255,255,1)"], [0, 1], 0, 0, 0, 130).drawRoundRect(0, 0, window.innerWidth, canvas.height, 5);
     var g = new createjs.Graphics();
     g.setStrokeStyle(1);
     g.beginStroke(createjs.Graphics.getRGB(0, 0, 0));
@@ -115,6 +84,7 @@ var drawGradient = function() {
 };
 
 var init = function() {
+
     drawGradient();
     drawMap();
     drawCharacters();
@@ -123,20 +93,20 @@ var init = function() {
 createjs.Ticker.addEventListener("tick", handleTick);
 function handleTick(event) {
  	for (var i = players.length - 1; i >= 0; i--) {
- 		var player = players[i]
- 		var image =  player.getImage()
+ 		var player = players[i];
+ 		var image =  player.getImage();
  		var target_y = player.getTargetBlock() * blockSize.getBounds().height / 2 - 50 + mapMargin;
 
  		var image = player.getImage();
  		image.y += 10;
- 		if(image.y >=target_y ) image.y = target_y
- 	};
+ 		if(image.y >=target_y ) image.y = target_y;
+ 	}
      stage.update();
 }
 
 init();
 
 var advanceOneStep = function(player){
-	var character = players[player]
-	character.setTargetBlock(character.getTargetBlock() + 1)
-}
+	var character = players[player];
+	character.setTargetBlock(character.getTargetBlock() + 1);
+};
